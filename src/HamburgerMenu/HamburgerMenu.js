2cf2e255236406fdcf69ffbce6146b15e8bac3c3
Jacob <jakeguss@gmail.com>
2019-10-23 15:41:46 -0600

    working on hamburger button

--- src/HamburgerMenu/HamburgerMenu.js
+++ src/HamburgerMenu/HamburgerMenu.js
@@ -1,25 +1,56 @@
 import React, { useState } from 'react';
 import router from '../router';
 
+import './HamburgerMenu.css'
+
 //import posed, { PoseGroup } from 'react-pose';
 import { Link } from 'react-router-dom';
 
 const HamburgerMenu = () => {
     
     const [menuOpen, setMenuOpen] = useState(false)
+
+    const handleMouseDown = e => {
+        setMenuOpen(!menuOpen)
+        console.log("clicked");
+        e.stopPropagation();
+    } 
+
+    let visibility = "hide";
+ 
+    if (menuOpen) {
+      visibility = "show";
+    }
     
     return (
-        <header className="main-logo">
-            <h1 id="name-header" alt="main logo">JACOB GUSS</h1>
-            <nav>
-                <ul className="nav-buttons">
-                    <li><Link className="link" to="/"><p>HOME</p></Link></li>
-                    <li><Link className="link" to="/portfolio"><p>PORTFOLIO</p></Link></li>
-                    <li><Link className="link" to="/skills"><p>SKILLS</p></Link></li>
-                    <li><Link className="link" to="/contact"><p>CONTACT</p></Link></li>
-                </ul>
-            </nav>
-        </header>    
+        <div>
+                <header className="main-logo">
+                <h1 id="name-header" alt="main logo">JACOB GUSS</h1>
+                <nav>
+                    <ul className="nav-buttons">
+                        <li><Link className="link" to="/"><p>HOME</p></Link></li>
+                        <li><Link className="link" to="/portfolio"><p>PORTFOLIO</p></Link></li>
+                        <li><Link className="link" to="/skills"><p>SKILLS</p></Link></li>
+                        <li><Link className="link" to="/contact"><p>CONTACT</p></Link></li>
+                    </ul>
+                </nav>
+                <button id="roundButton" onMouseDown={handleMouseDown}></button>
+                {/* <nav>
+                    <ul className="nav-buttons">
+                        <li><Link className="link" to="/"><p>HOME</p></Link></li>
+                        <li><Link className="link" to="/portfolio"><p>PORTFOLIO</p></Link></li>
+                        <li><Link className="link" to="/skills"><p>SKILLS</p></Link></li>
+                        <li><Link className="link" to="/contact"><p>CONTACT</p></Link></li>
+                    </ul>
+                </nav> */}
+            </header>
+            <div id="flyoutMenu" onMouseDown={handleMouseDown} className={visibility}>
+                <li><Link className="link" to="/"><p>HOME</p></Link></li>
+                <li><Link className="link" to="/portfolio"><p>PORTFOLIO</p></Link></li>
+                <li><Link className="link" to="/skills"><p>SKILLS</p></Link></li>
+                <li><Link className="link" to="/contact"><p>CONTACT</p></Link></li>
+            </div>
+        </div> 
     );
     
 }
