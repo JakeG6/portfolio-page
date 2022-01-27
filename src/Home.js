c43bdf4bddfa57ec2e61fda2034e3dacae1dbf18
Jake <jakeguss@gmail.com>
2022-01-26 18:16:44 -0800

    updated biography. updated link to resume.

--- src/Home.js
+++ src/Home.js
@@ -36,7 +36,7 @@ class Home extends Component {
           <img className="profile-picture" src="Images/rainforest1.jpg" alt="A forest"/>
           <p className="about-me">Hi, I'm a full-stack developer with over four years of experience who loves javascript-based web technologies and developing intricate programmatic systems. 
             On this site you'll find my various projects and contact links. I'm excited to continue learning and putting all my skills to use.
-            <a href="https://www.dropbox.com/s/9ycagfflx36bkhw/JGuss_WebDev_Resume_Revised.pdf?dl=0" target="_blank" className="resume-button"> You can find my resume here!</a>
+            <a href="https://www.dropbox.com/s/ykxz54qpab74s4i/JGuss_WebDev_Resume_V3.pdf?dl=0" target="_blank" className="resume-button"> You can find my resume here!</a>
           </p>
         </section>
         < Portfolio />
